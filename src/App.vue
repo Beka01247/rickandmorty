0e6489980bb4fa872f1bfc35e3e101f3ec3f8899
Bekarys Serikov <bekaryss03@email.com>
2024-05-30 01:20:17 +0500

    feat: update css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,132 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="top">
+      <h1>Rick and Morty Characters</h1>
+    </div>
+    <div>
+      <input v-model="filters.name" placeholder="Name">
+      <select v-model="filters.status">
+        <option value="">Any Status</option>
+        <option value="alive">Alive</option>
+        <option value="dead">Dead</option>
+        <option value="unknown">Unknown</option>
+      </select>
+      <button @click="applyFilters">Apply</button>
+    </div>
+    <div class="cards-container">
+      <CharacterCard
+        v-for="character in characters"
+        :key="character.id"
+        :character="character"
+      />
+    </div>
+    <div>
+      <button @click="changePage(-1)" :disabled="!prev">Prev</button>
+      <button @click="changePage(1)" :disabled="!next">Next</button>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, reactive, watchEffect } from 'vue';
+import CharacterCard from './components/CharacterCard.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const apiUrl = 'https://rickandmortyapi.com/api/character';
+const characters = ref([]);
+const prev = ref(null);
+const next = ref(null);
+
+const filters = reactive({
+  name: '',
+  status: ''
+});
+
+const fetchData = async () => {
+  let url = `${apiUrl}?name=${filters.name}&status=${filters.status}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  characters.value = data.results;
+  prev.value = data.info.prev;
+  next.value = data.info.next;
+};
+
+watchEffect(() => {
+  fetchData();
+});
+
+const applyFilters = () => {
+  fetchData();
+};
+
+const changePage = async (direction) => {
+  const pageUrl = direction === 1 ? next.value : prev.value;
+  if (pageUrl) {
+    const response = await fetch(pageUrl);
+    const data = await response.json();
+    characters.value = data.results;
+    prev.value = data.info.prev;
+    next.value = data.info.next;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+
+body {
+  background-color: rgb(39, 43, 51);
+  font-family: "Rubik", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-style: normal;
+  margin: 0px !important;
+}
+
+.top {
+  background: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  margin: 0px;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  
+  margin: 20px;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:disabled {
+  background-color: #a5a5a5;
+}
+
+input, select {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
 }
 </style>
+
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card">
+    <img :src="character.image" alt="character.name">
+    <div>
+    <h2>{{ character.name }}</h2>
+    <p><span class="prop" style="margin-top: 16px;">Status: </span> {{ character.status }}</p>
+    <p><span class="prop">Species:</span> {{ character.species }}</p>
+    <p><span class="prop">Gender:</span> {{ character.gender }}</p>
+    <!-- Вместо ссылок, выводим текст -->
+    <p><span class="prop">Origin:</span> {{ character.origin.name }}</p>
+    <p><span class="prop">Last known location:</span> {{ character.location.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    character: Object
+  }
+}
+</script>
+
+<style>
+
+.prop {
+  color: rgb(158, 158, 158);
+  margin-right: 4px;
+}
+.card {
+  display: flex;
+  background-color: #3c3e44;
+  color: white;
+  border-radius: 10px;
+  width: 47%;
+  height: 207px;
+  overflow: hidden;
+  margin: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  scale: 1;
+  transition-duration: 0.5s;
+}
+
+.card:hover {
+  scale: 1.04;
+}
+
+p, h2 {
+  margin: 4px;
+}
+
+h2 {
+  margin-bottom: 16px;
+}
+
+.card img {
+  object-fit: cover;
+}
+
+.card div {
+  padding: 10px;
+}
+</style>
+
